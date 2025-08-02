22364a92050a84560c395998df1fc1d87f77180f
vicenterusso <vicente.russo@gmail.com>
2025-08-02 21:32:20 +0000

    Deploying to gh-pages from @ isar-private-testing/isar_community@cc21208a3920713dd6efed953ce5b60c23fafbb3 🚀

--- 3.2.0-dev.1/flutter_bootstrap.js
+++ 3.2.0-dev.1/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3871393269"
+    serviceWorkerVersion: "2709161761"
   }
 });

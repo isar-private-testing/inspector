1f992bce7ad8512ab8b959aa9cef5c22368d04a1
vicenterusso <vicente.russo@gmail.com>
2025-08-05 01:44:40 +0000

    Deploying to gh-pages from @ isar-private-testing/isar_community@daef6ff2c9b156d20911337b714d5c0e474aae55 🚀

--- 3.2.0-dev.1/flutter_service_worker.js
+++ 3.2.0-dev.1/flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c26b6d43cae29261168fec1d9b1eca1b
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "15d54d142da2f2d6f2e90ed1d55121af",
 "version.json": "4dfbf0b24819d40215119161403e064a",
 "manifest.json": "f61ca3214458e7834b056aaf87148a73",
-"flutter_bootstrap.js": "1e139a8d07ed3bba6ee745868831abba",
+"flutter_bootstrap.js": "0240bd0e66c49ccb6ffa154bd9eddc61",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "c26b6d43cae29261168fec1d9b1eca1b
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
-"index.html": "072cd3cee3a5e312cddc6c166ee90522",
-"/": "072cd3cee3a5e312cddc6c166ee90522",
+"index.html": "5d6ca147ac76e0bfafb7795f0d468aac",
+"/": "5d6ca147ac76e0bfafb7795f0d468aac",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "main.dart.js": "d8e8a81f23f90324fc7619a97115a202"};
 // The application shell files that are downloaded before a service worker can
